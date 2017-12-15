2e7c4f77f52636e2ce24f1f553d85adc145bc436
CriptoPozol+DDHH <criptopozol@protonmail.com>
2017-12-14 19:01:11 -0600

    Eliminar código muerto

--- script.js
+++ script.js
@@ -111,19 +111,3 @@ var diariosChart = new Chart(diariosCtx, {
         }
     }
 });
-/*
-var data = [
-    ["", "Ford", "Tesla", "Toyota", "Honda"],
-    ["2017", 10, 11, 12, 13],
-    ["2018", 20, 11, 14, 13],
-    ["2019", 30, 15, 12, 13]
-];
-
-var container = document.getElementById('data');
-var hot = new Handsontable(container, {
-    data: data,
-    readOnly: true,
-    rowHeaders: true,
-    colHeaders: true
-});
-*/
