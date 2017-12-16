0d2ab931b6bde883414249f3e10b9cd38fcc2002
CriptoPozol+DDHH <criptopozol@protonmail.com>
2017-12-16 09:12:07 -0600

    Agregar mapa con porcentajes por estado

--- mapa.js
+++ mapa.js
@@ -1,13 +1,3 @@
-var mapboxAccessToken = "pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw";
-var map = L.map('mapid').setView([19.432608, -99.133209], 4);
-
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=' + mapboxAccessToken, {
-    id: 'mapbox.light',
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-	'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-	'Imagery © <a href="http://mapbox.com">Mapbox</a>'
-}).addTo(map);
-
 function state_data(nombre, apoyos, porcentaje)
 {
     this.nombre = nombre;
@@ -53,28 +43,61 @@ var state_datas = [
 var i = 0;
 for (state of statesData.features) {
     // sanity check
-    if (state.properties["name"] == state_datas[i].nombre)
-	state.properties["apoyos"] = state_datas[i].porcentaje;
+    if (state.properties["name"] == state_datas[i].nombre) {
+	state.properties["nombre"] = state_datas[i].nombre;
+	state.properties["apoyos"] = state_datas[i].apoyos;
+	state.properties["porcentaje"] = state_datas[i].porcentaje;
+    }
     i += 1;
 }
 
+var mapboxAccessToken = "pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw";
+var map = L.map('mapid').setView([19.432608, -99.133209], 4);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=' + mapboxAccessToken, {
+    id: 'mapbox.light',
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+	'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+	'Imagery © <a href="http://mapbox.com">Mapbox</a>'
+}).addTo(map);
+
+// control that shows state info on hover
+var info = L.control();
+
+info.onAdd = function (map) {
+    this._div = L.DomUtil.create('div', 'info');
+    this.update();
+    return this._div;
+};
+
+info.update = function (props) {
+    this._div.innerHTML = '<h4>Apoyos por Estado</h4>' +
+	(props ?
+	 '<b>' + props.nombre + '</b><br />' +
+	 props.apoyos + ' apoyos'+ '<br />' +
+	 props.porcentaje + '%'
+	 : 'Pon el mouse sobre un estado');
+};
+
+info.addTo(map);
+
 function getColor(d) {
-    return d >= 10 ? "#00441b":
-	d > 9 ? "#006d2c":
-	d > 8 ? "#238b45":
-	d > 7 ? "#41ab5d":
-	d > 6 ? "#74c476":
-	d > 5 ? "#a1d99b":
-	d > 4 ? "#c7e9c0":
-	d > 3 ? "#e5f5e0":
-	d > 2 ? "#f7fcf5":
-	d > 1 ? "#fffcf5":
+    return d >= 30 ? "#00441b":
+	d > 27 ? "#006d2c":
+	d > 24 ? "#238b45":
+	d > 21 ? "#41ab5d":
+	d > 18 ? "#74c476":
+	d > 15 ? "#a1d99b":
+	d > 12 ? "#c7e9c0":
+	d > 9 ? "#e5f5e0":
+	d > 6 ? "#f7fcf5":
+	d > 3 ? "#fffcf5":
 	"#ffffff";
 }
 
 function style(feature) {
     return {
-        fillColor: getColor(feature.properties.apoyos),
+        fillColor: getColor(feature.properties.porcentaje),
         weight: 2,
         opacity: 1,
         color: 'white',
@@ -83,4 +106,68 @@ function style(feature) {
     };
 }
 
-L.geoJson(statesData, {style: style}).addTo(map);
+function highlightFeature(e) {
+    var layer = e.target;
+
+    layer.setStyle({
+	weight: 5,
+	color: '#666',
+	dashArray: '',
+	fillOpacity: 0.7
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+	layer.bringToFront();
+    }
+
+    info.update(layer.feature.properties);
+}
+
+var geojson;
+
+function resetHighlight(e) {
+    geojson.resetStyle(e.target);
+    info.update();
+}
+
+function zoomToFeature(e) {
+    map.fitBounds(e.target.getBounds());
+}
+
+function onEachFeature(feature, layer) {
+    layer.on({
+	mouseover: highlightFeature,
+	mouseout: resetHighlight,
+	click: zoomToFeature
+    });
+}
+
+geojson = L.geoJson(statesData, {
+    style: style,
+    onEachFeature: onEachFeature
+}).addTo(map);
+
+map.attributionControl.addAttribution('Con datos del INE <a href="http://www.ine.mx/reportes-apoyo-ciudadano/">Reportes de Apoyo Ciudadano</a>');
+
+var legend = L.control({position: 'bottomleft'});
+
+legend.onAdd = function (map) {
+    var div = L.DomUtil.create('div', 'info legend'),
+	grades = [0, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30],
+	labels = [],
+	from, to;
+
+    for (var i = 0; i < grades.length; i++) {
+	from = grades[i];
+	to = grades[i + 1];
+
+	labels.push(
+	    '<i style="background:' + getColor(from + 1) + '"></i> ' +
+		from + (to ? '&ndash;' + to : '+') + '%');
+    }
+
+    div.innerHTML = labels.join('<br>');
+    return div;
+};
+
+legend.addTo(map);
