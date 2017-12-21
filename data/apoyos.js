573b7a4e4ae673df50cb77a0846d93ec78aaeca0
CriptoPozol+DDHH <32035767+criptopozol@users.noreply.github.com>
2017-12-21 15:34:21 -0600

    Actualización 2017-12-21

--- data/apoyos.js
+++ data/apoyos.js
@@ -48,4 +48,5 @@ var apoyos_data = [
     {"fecha":"2017-12-18","apoyos":106519,"auxiliares":8441,"auxiliares_activos":3372},
     {"fecha":"2017-12-19","apoyos":108535,"auxiliares":8442,"auxiliares_activos":3410},
     {"fecha":"2017-12-20","apoyos":110420,"auxiliares":8521,"auxiliares_activos":3427},
+    {"fecha":"2017-12-21","apoyos":112323,"auxiliares":8521,"auxiliares_activos":3448},
 ];
